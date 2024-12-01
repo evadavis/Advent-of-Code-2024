5d539441db179ca381d507adee45699512bf73dd
evadavis <evad2003@gmail.com>
2024-12-01 21:21:24 +0000

    Day 1 AOC

---
+++ day1.py
@@ -0,0 +1,27 @@
+first = []
+second = []
+totaldistance = 0
+with open(r"C:\Users\evad2\OneDrive\Documents\AOC\input.txt", "r") as listfile:
+    for line in listfile:
+        splited = line.split()
+        first.append(int(splited[0]))
+        second.append(int(splited[1]))
+
+
+first.sort()
+second.sort()
+check = {}
+for i in range(0, len(second)):
+    x = check.get(second[i])
+    if x is None:
+        check[second[i]] = 1
+    else:
+        check[second[i]] += 1
+
+print(check)
+total = 0
+for i in first:
+    if check.get(i) != None:
+        total += i * check.get(i)
+
+print(total)
